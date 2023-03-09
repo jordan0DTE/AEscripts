c6f85feec973466b685c7c4980656d8d6658a0c5
jordan0DTE <109185123+jordan0DTE@users.noreply.github.com>
2023-03-09 03:14:32 -0800

    Add files via upload

---
+++ MaskLocksmith_Dockable.jsx
@@ -0,0 +1,91 @@
+﻿(function(thisObj) {
+
+// Any code you write here will execute before the panel is built.
+
+buildUI(thisObj); // Calling the function to build the panel
+
+‍
+
+function buildUI(thisObj) {
+
+var windowName = "your script name";
+
+  var myPanel = (thisObj instanceof Panel) ? thisObj : new Window("window", windowName, undefined, {
+          resizeable: true
+     });
+
+      // Write any UI code here. Note: myPanel is your window panel object. If you add groups, buttons, or other UI objects make sure you use myPanel as their parent object, especially if you are using the only ScriptUI panel builder.
+// Add a "Lock Masks" button to the window
+var lockButton = myPanel.add("button", undefined, "Lock Masks");
+
+// Add an event listener that runs the lockMasks function when the button is clicked
+lockButton.onClick = function() {
+  app.beginUndoGroup("My Undo Group");
+  var selectedLayers = app.project.activeItem.selectedLayers;
+  if (selectedLayers.length > 0) {
+    // Iterate through each selected layer
+    for (var i = 0; i < selectedLayers.length; i++) {
+      var selectedLayer = selectedLayers[i];
+
+      // Get the mask parade for the selected layer
+      var maskParade = selectedLayer.property("ADBE Mask Parade");
+    
+      // Iterate through each mask in the mask parade
+      for (var j = 1; j <= maskParade.numProperties; j++) {
+        // Lock the current mask
+        var mask = maskParade.property(j);
+        mask.locked = true;
+      }
+    }
+  }
+  app.endUndoGroup();
+}
+
+// Add an "Unlock Masks" button to the window
+var unlockButton = myPanel.add("button", undefined, "Unlock Masks");
+
+// Add an event listener that runs the unlockMasks function when the button is clicked
+unlockButton.onClick = function() {
+  app.beginUndoGroup("My Undo Group");
+  var selectedLayers = app.project.activeItem.selectedLayers;
+  if (selectedLayers.length > 0) {
+    // Iterate through each selected layer
+    for (var i = 0; i < selectedLayers.length; i++) {
+      var selectedLayer = selectedLayers[i];
+
+      // Get the mask parade for the selected layer
+      var maskParade = selectedLayer.property("ADBE Mask Parade");
+    
+      // Iterate through each mask in the mask parade
+      for (var j = 1; j <= maskParade.numProperties; j++) {
+        // Unock the current masks
+        var mask = maskParade.property(j);
+        mask.locked = false;
+      }
+    }
+  }
+  app.endUndoGroup();
+}
+
+‍
+     myPanel.onResizing = myPanel.onResize = function() {
+        this.layout.resize();
+     };
+     if (myPanel instanceof Window) {
+        myPanel.center();
+        myPanel.show();
+     } else {
+        myPanel.layout.layout(true);
+        myPanel.layout.resize();
+     }
+
+}
+
+// Write your helper functions here
+
+})(this);
+
+
+
+
+
---
+++ MaskLocksmith_Kbar.jsx
@@ -0,0 +1,53 @@
+﻿var selectedLayers = app.project.activeItem.selectedLayers;
+var myState = ScriptUI.environment.keyboardState;
+app.beginUndoGroup("My Undo Group");
+function lockMasks() {
+if (selectedLayers.length > 0) {
+  // Iterate through each selected layer
+  for (var i = 0; i < selectedLayers.length; i++) {
+    var selectedLayer = selectedLayers[i];
+
+    // Get the mask parade for the selected layer
+    var maskParade = selectedLayer.property("ADBE Mask Parade");
+    
+    // Iterate through each mask in the mask parade
+    for (var j = 1; j <= maskParade.numProperties; j++) {
+      // Lock the current mask
+      var mask = maskParade.property(j);
+      mask.locked = true;
+      }
+  }
+}
+}
+app.endUndoGroup();
+
+app.beginUndoGroup("My Other Undo Group");
+function unlockMasks() {
+if (selectedLayers.length > 0) {
+  // Iterate through each selected layer
+  for (var i = 0; i < selectedLayers.length; i++) {
+    var selectedLayer = selectedLayers[i];
+
+    // Get the mask parade for the selected layer
+    var maskParade = selectedLayer.property("ADBE Mask Parade");
+    
+    // Iterate through each mask in the mask parade
+    for (var j = 1; j <= maskParade.numProperties; j++) {
+      // Unock the current masks
+      var mask = maskParade.property(j);
+      mask.locked = false;
+      }
+  }
+}
+}
+app.endUndoGroup();
+
+app.beginUndoGroup("My Last Undo Group");
+if (myState.shiftKey) {
+   unlockMasks ();
+}
+else
+{
+    lockMasks ();
+    }      
+app.endUndoGroup();
